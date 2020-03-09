fb7c62a39fee5cec52afe8de5e27af3c918434dd
yejiayiregwoo@gmail.com <yejy.jx@founder.com.cn>
2020-03-09 15:53:31 -0400

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\11038\OneDrive\Documents\Django_demo\mysite\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
